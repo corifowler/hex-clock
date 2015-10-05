c7c5076a6fa4f5c29fc6f784ceb324ce46edcef9
Cori Fowler <fowler.cori@gmail.com>
2015-10-05 17:34:03 -0400

    made letters uppercase

--- js/main.js
+++ js/main.js
@@ -8,13 +8,13 @@ setInterval( function() {
   var mins = (min < 10) ? ('0' + min) : min;
   var seconds = (secs < 10) ? ('0' + secs) : secs;
 
-  var hexHrs = hrs.toString(16);
-  var hexMins = mins.toString(16);
-  var hexSeconds = seconds.toString(16);
+  var hexHrs = hrs.toString(16).toUpperCase();
+  var hexMins = mins.toString(16).toUpperCase();
+  var hexSeconds = seconds.toString(16).toUpperCase();
 
-  var hexHours = (hexHrs.value > 9) || (hexHrs === 'a') || (hexHrs === 'b') || (hexHrs === 'c') || (hexHrs === 'd') || (hexHrs === 'e') || (hexHrs === 'f') ? ('0' + hexHrs) : hexHrs;
-  var hexMinutes = (hexMins === 'a') || (hexMins === 'b') || (hexMins === 'c') || (hexMins === 'd') || (hexMins === 'e') || (hexMins === 'f') ? ('0' + hexMins) : hexMins;
-  var hexSecs = (hexSeconds === 'a') || (hexSeconds === 'b') || (hexSeconds === 'c') || (hexSeconds === 'd') || (hexSeconds === 'e') || (hexSeconds === 'f') ? ('0' + hexSeconds) : hexSeconds;
+  var hexHours = (hexHrs === 'A') || (hexHrs === 'B') || (hexHrs === 'C') || (hexHrs === 'D') || (hexHrs === 'E') || (hexHrs === 'F') ? ('0' + hexHrs) : hexHrs;
+  var hexMinutes = (hexMins === 'A') || (hexMins === 'B') || (hexMins === 'C') || (hexMins === 'D') || (hexMins === 'E') || (hexMins === 'F') ? ('0' + hexMins) : hexMins;
+  var hexSecs = (hexSeconds === 'A') || (hexSeconds === 'B') || (hexSeconds === 'C') || (hexSeconds === 'D') || (hexSeconds === 'E') || (hexSeconds === 'F') ? ('0' + hexSeconds) : hexSeconds;
 
   var hexTimeBg = '#' + hexHours + hexMinutes + hexSecs;
   var hexTime = hexHours + hexMinutes + hexSecs;
